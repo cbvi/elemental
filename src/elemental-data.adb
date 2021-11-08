9efe259d8fa207db1519eb11139d01d77552b4e4
Carlin Bingham <cb@viennan.net>
2021-11-09 05:13:54 +1300

    Use helper procedure for getting attributes

--- src/elemental-data.adb
+++ src/elemental-data.adb
@@ -4,18 +4,38 @@ with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 package body Elemental.Data is
 
+   procedure Get_Attribute_By_Name
+     (Handler : in out Elemental.PageReader.Reader;
+      Atts    :        Sax.Readers.Sax_Attribute_List;
+      Name    :        String;
+      Value   : in out UB.Unbounded_String;
+      Found   : in out Boolean)
+   is
+      Index : Integer;
+   begin
+      Index := Sax.Readers.Get_Index (Handler, Atts, "", Name);
+
+      if Index /= -1 then
+         Value := UB.To_Unbounded_String
+           (Sax.Symbols.Get (Sax.Readers.Get_Value (Atts, Index)).all);
+         Found := True;
+      else
+         Found := False;
+      end if;
+   end Get_Attribute_By_Name;
+
    function Get_Title
       (Handler    : in out Elemental.PageReader.Reader;
        Atts       : Sax.Readers.Sax_Attribute_List)
        return UB.Unbounded_String
    is
-      Index : Integer;
+      Title : UB.Unbounded_String;
+      Found : Boolean := False;
    begin
-      Index := Sax.Readers.Get_Index (Handler, Atts, "", "title");
+      Get_Attribute_By_Name (Handler, Atts, "title", Title, Found);
 
-      if Index /= -1 then
-         return UB.To_Unbounded_String
-           (Sax.Symbols.Get (Sax.Readers.Get_Value (Atts, Index)).all);
+      if Found then
+         return Title;
       else
          raise Elemental.PageReader.Page_Error with "Page must have title";
       end if;
@@ -25,27 +45,26 @@ package body Elemental.Data is
       (Handler    : in out Elemental.PageReader.Reader;
        Atts       : Sax.Readers.Sax_Attribute_List)
    is
-      Index    : Integer;
-      Name     : UB.Unbounded_String;
+      Source   : UB.Unbounded_String;
+      What     : UB.Unbounded_String;
+      Found    : Boolean := False;
       Fragment : Elemental.Page.Fragment (Elemental.Page.External);
    begin
-      Index := Sax.Readers.Get_Index (Handler, Atts, "", "source");
-      if Index /= -1 then
-         Name := UB.To_Unbounded_String
-           (Sax.Symbols.Get (Sax.Readers.Get_Value (Atts, Index)).all);
-         Fragment.Source := Name;
+      Get_Attribute_By_Name (Handler, Atts, "source", Source, Found);
+
+      if Found then
+         Fragment.Source := Source;
       else
          raise Elemental.PageReader.Page_Error with "Fragment must have source";
       end if;
 
-      Index := Sax.Readers.Get_Index (Handler, Atts, "", "type");
-      if Index /= -1 then
-         Name := UB.To_Unbounded_String
-           (Sax.Symbols.Get (Sax.Readers.Get_Value (Atts, Index)).all);
+      Found := False;
+      Get_Attribute_By_Name (Handler, Atts, "type", What, Found);
 
-         if Name = "code" then
+      if Found then
+         if What = "code" then
             Fragment.What := Elemental.Page.Code;
-         elsif Name = "html" then
+         elsif What = "html" then
             Fragment.What := Elemental.Page.Html;
          else
             Fragment.What := Elemental.Page.Text;
@@ -53,7 +72,6 @@ package body Elemental.Data is
       else
          Fragment.What := Elemental.Page.Text;
       end if;
-
       Handler.Page.Fragments.Append (Fragment);
    end Process_Fragment;
 end Elemental.Data;
--- src/elemental-data.ads
+++ src/elemental-data.ads
@@ -5,6 +5,13 @@ with Elemental.PageReader;
 package Elemental.Data is
    package UB renames Ada.Strings.Unbounded;
 
+   procedure Get_Attribute_By_Name
+     (Handler : in out Elemental.PageReader.Reader;
+      Atts    :        Sax.Readers.Sax_Attribute_List;
+      Name    :        String;
+      Value   : in out UB.Unbounded_String;
+      Found   : in out Boolean);
+
    function Get_Title
       (Handler    : in out Elemental.PageReader.Reader;
        Atts       : Sax.Readers.Sax_Attribute_List)
