8d550c29c4ae92972312a800eac4baec2d972f14
Carlin Bingham <cb@viennan.net>
2021-11-15 03:04:25 +1300

    Start putting it all together

--- src/main.adb
+++ src/main.adb
@@ -1,25 +1,56 @@
+with Elemental.SettingsReader;
+with Elemental.IndexReader;
 with Elemental.PageReader;
+with Elemental.Page;
 with Input_Sources.File;
+with Ada.Strings;
+with Ada.Strings.Unbounded;
+with Ada.Text_IO;
 
 procedure Main is
-   My_Reader : Elemental.PageReader.Reader;
-   Input     : Input_Sources.File.File_Input;
+   package UB renames Ada.Strings.Unbounded;
+   function TS (S : UB.Unbounded_String)
+                return String renames UB.To_String;
+
+   Settings_File   : Input_Sources.File.File_Input;
+   Settings_Reader : Elemental.SettingsReader.Reader;
+
+   Index_File   : Input_Sources.File.File_Input;
+   Index_Reader : Elemental.IndexReader.Reader;
+
+   Page_File   : Input_Sources.File.File_Input;
+   Page_Reader : Elemental.PageReader.Reader;
 begin
-   Input_Sources.File.Open ("test/basic.xml", Input);
+   Input_Sources.File.Open ("test/setset/settings.xml", Settings_File);
+   Elemental.SettingsReader.Parse (Settings_Reader, Settings_File);
+   Input_Sources.File.Close (Settings_File);
 
-   Elemental.PageReader.Parse (My_Reader, Input);
+   Input_Sources.File.Open (TS (Settings_Reader.Settings.Pages), Index_File);
+   Elemental.IndexReader.Parse (Index_Reader, Index_File);
+   Input_Sources.File.Close (Index_File);
 
-   Input_Sources.File.Close (Input);
+   for S of Index_Reader.Pages loop
+      Input_Sources.File.Open (TS (S), Page_File);
+      Elemental.PageReader.Parse (Page_Reader, Page_File);
+      Input_Sources.File.Close (Page_File);
 
-   Input_Sources.File.Open ("test/transclude.xml", Input);
-   Elemental.PageReader.Parse (My_Reader, Input);
-   Input_Sources.File.Close (Input);
+      Ada.Text_IO.Put_Line (TS (Page_Reader.Page.Title));
 
-   Input_Sources.File.Open ("test/mixed.xml", Input);
-   Elemental.PageReader.Parse (My_Reader, Input);
-   Input_Sources.File.Close (Input);
+      declare
+         New_File : Ada.Text_IO.File_Type;
+         New_Html : UB.Unbounded_String;
+      begin
+         Ada.Text_IO.Create
+           (New_File,
+            Ada.Text_IO.Out_File,
+            TS (Settings_Reader.Settings.Output) & TS (S) & ".html",
+            "WCEM=8");
+         New_Html := Elemental.Page.To_Html
+           (Page_Reader.Page, TS (Settings_Reader.Settings.Template));
+         Ada.Text_IO.Put (New_File, TS (New_Html));
+         Ada.Text_IO.Close (New_File);
+      end;
 
-   Input_Sources.File.Open ("test/unicode.xml", Input);
-   Elemental.PageReader.Parse (My_Reader, Input);
-   Input_Sources.File.Close (Input);
+      Page_Reader.Page.Fragments.Clear;
+   end loop;
 end Main;
