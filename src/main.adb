b5525205e04c99d37e1eda3d5523ea1eefd958c4
Carlin Bingham <cb@viennan.net>
2021-11-21 01:51:34 +1300

    Use helper functions

--- src/main.adb
+++ src/main.adb
@@ -1,8 +1,6 @@
-with Elemental.SettingsReader;
-with Elemental.IndexReader;
-with Elemental.PageReader;
+with Elemental.Index;
+with Elemental.Settings;
 with Elemental.Page;
-with Input_Sources.File;
 with Ada.Strings;
 with Ada.Strings.Unbounded;
 with Ada.Text_IO;
@@ -12,29 +10,18 @@ procedure Main is
    function TS (S : UB.Unbounded_String)
                 return String renames UB.To_String;
 
-   Settings_File   : Input_Sources.File.File_Input;
-   Settings_Reader : Elemental.SettingsReader.Reader;
-
-   Index_File   : Input_Sources.File.File_Input;
-   Index_Reader : Elemental.IndexReader.Reader;
-
-   Page_File   : Input_Sources.File.File_Input;
-   Page_Reader : Elemental.PageReader.Reader;
+   Settings    : Elemental.Settings.Settings;
+   Index       : Elemental.Index.List;
+   Page        : Elemental.Page.Page;
 begin
-   Input_Sources.File.Open ("test/setset/settings.xml", Settings_File);
-   Elemental.SettingsReader.Parse (Settings_Reader, Settings_File);
-   Input_Sources.File.Close (Settings_File);
+   Elemental.Settings.Process_Settings ("test/setset/settings.xml", Settings);
 
-   Input_Sources.File.Open (TS (Settings_Reader.Settings.Pages), Index_File);
-   Elemental.IndexReader.Parse (Index_Reader, Index_File);
-   Input_Sources.File.Close (Index_File);
+   Elemental.Index.Get_Pages (TS (Settings.Pages), Index);
 
-   for S of Index_Reader.Pages loop
-      Input_Sources.File.Open (TS (S), Page_File);
-      Elemental.PageReader.Parse (Page_Reader, Page_File);
-      Input_Sources.File.Close (Page_File);
+   for S of Index loop
+      Page := Elemental.Page.Get_Page (TS (S));
 
-      Ada.Text_IO.Put_Line (TS (Page_Reader.Page.Title));
+      Ada.Text_IO.Put_Line (TS (Page.Title));
 
       declare
          New_File : Ada.Text_IO.File_Type;
@@ -43,14 +30,14 @@ begin
          Ada.Text_IO.Create
            (New_File,
             Ada.Text_IO.Out_File,
-            TS (Settings_Reader.Settings.Output) & TS (S) & ".html",
+            TS (Settings.Output) & TS (S) & ".html",
             "WCEM=8");
          New_Html := Elemental.Page.To_Html
-           (Page_Reader.Page, TS (Settings_Reader.Settings.Template));
+           (Page, TS (Settings.Template));
          Ada.Text_IO.Put (New_File, TS (New_Html));
          Ada.Text_IO.Close (New_File);
       end;
 
-      Page_Reader.Page.Fragments.Clear;
+      Page.Fragments.Clear;
    end loop;
 end Main;
